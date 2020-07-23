aeaeec97e5d672bf06caab535cd9176cc85db5bd
sakshi111ru <62183753+sakshi111ru@users.noreply.github.com>
2020-07-23 04:14:48 -0700

    Add files via upload

---
+++ lib/screens/add_student.dart
@@ -0,0 +1,45 @@
+import 'package:dbDemo_app/models/student.dart';
+import 'package:dbDemo_app/services/student_operations.dart';
+import 'package:flutter/material.dart';
+
+class AddStudent extends StatelessWidget {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController branchController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Student'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: <Widget>[
+          TextField(
+            controller: nameController,
+            decoration: InputDecoration(hintText: 'Name'),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            controller: branchController,
+            decoration: InputDecoration(hintText: 'Branch'),
+          ),
+          SizedBox(height: 16),
+          RaisedButton(
+            child: Text('AddStudent'),
+            onPressed: () async {
+              String name = nameController.text;
+              String branch = branchController.text;
+
+              Student s = Student(name: name, branch: branch);
+
+              await StudentOperations.instance.addStudent(s);
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
---
+++ lib/screens/edit_screen.dart
@@ -0,0 +1,55 @@
+import 'package:dbDemo_app/models/student.dart';
+import 'package:dbDemo_app/services/student_operations.dart';
+import 'package:flutter/material.dart';
+
+class EditStudent extends StatelessWidget {
+  final Student student;
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController branchController = TextEditingController();
+
+  EditStudent({this.student});
+
+  @override
+  Widget build(BuildContext context) {
+    // set values to controller
+    nameController.text = student.name;
+    branchController.text = student.branch;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Student'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: <Widget>[
+          TextField(
+            controller: nameController,
+            decoration: InputDecoration(
+              hintText: 'Name',
+            ),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            controller: branchController,
+            decoration: InputDecoration(
+              hintText: 'Branch',
+            ),
+          ),
+          SizedBox(height: 16),
+          RaisedButton(
+            child: Text('Edit Student'),
+            onPressed: () async {
+              String name = nameController.text;
+              String branch = branchController.text;
+
+              Student s = Student(id: student.id, name: name, branch: branch);
+              await StudentOperations.instance.editStudent(s);
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
